fb551227fee0e16b60d17dc8452034e2ab5c4e0d
Mike Rachman <mikerachman1@gmail.com>
2022-07-12 10:44:10 -0400

    Added #path method to Queen class

--- lib/queen.rb
+++ lib/queen.rb
@@ -2,7 +2,7 @@ class Queen
   attr_accessor :location
   attr_reader :color
   def initialize(location, color)
-    @location = location,
+    @location = location
     @color = color
   end
 
@@ -23,7 +23,73 @@ class Queen
     end
     result
   end
+
+  def path(start, destination, path = [])
+    possibilities = possible_moves(start)
+    return 'ERROR move impossible' unless possibilities.include?(destination)
+
+    down_right = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]]
+    up_left = [[-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]]
+    down_left = [[1, -1], [2, -2], [3,-3], [4, -4], [5, -5], [6, -6], [7, -7]]
+    up_right = [[-1, 1], [-2, 2], [-3,3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]]
+    down = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
+    right = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
+    up = [[-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]]
+    left = [[0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7]]
+
+    if start[0] < destination[0] && start[1] < destination[1] #down_right
+      down_right.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] > destination[0] && start[1] > destination[1] #up_left
+      up_left.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] < destination[0] && start[1] > destination[1] #down_left
+      down_left.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] > destination[0] && start[1] < destination[1] #up_right
+      up_right.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] < destination[0] && start[1] == destination[1] #down
+      down.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[1] < destination[1] && start[0] == destination[0] #right
+      right.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] > destination[0] && start[1] == destination[1] #up
+      up.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[1] > destination[1] && start[0] == destination[0] #left
+      left.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    end
+    path.unshift(start)
+    path.push(destination)
+  end
 end
 
 # queen = Queen.new([0, 3], 'b')
-# p queen.possible_moves([0, 3])
+# p queen.path([4,4], [6,3])
