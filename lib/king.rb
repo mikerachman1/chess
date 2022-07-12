4377a3bc1f35d282bbb9ccf966dc56c28844238e
Mike Rachman <mikerachman1@gmail.com>
2022-07-12 10:51:32 -0400

    Added #path method to King class

--- lib/king.rb
+++ lib/king.rb
@@ -2,7 +2,7 @@ class King
   attr_accessor :location
   attr_reader :color
   def initialize(location, color)
-    @location = location,
+    @location = location
     @color = color
   end
 
@@ -17,7 +17,73 @@ class King
     end
     result
   end
+
+  def path(start, destination, path = [])
+    possibilities = possible_moves(start)
+    return 'ERROR move impossible' unless possibilities.include?(destination)
+
+    down = [[1, 0]]
+    right = [[0, 1]]
+    up = [[-1, 0]]
+    left = [[0, -1]]
+    down_right = [[1, 1]]
+    up_left = [[-1, -1]]
+    down_left = [[1, -1]]
+    up_right = [[-1, 1]]
+
+    if start[0] < destination[0] && start[1] < destination[1] #down_right
+      down_right.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] > destination[0] && start[1] > destination[1] #up_left
+      up_left.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] < destination[0] && start[1] > destination[1] #down_left
+      down_left.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] > destination[0] && start[1] < destination[1] #up_right
+      up_right.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] < destination[0] && start[1] == destination[1] #down
+      down.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[1] < destination[1] && start[0] == destination[0] #right
+      right.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[0] > destination[0] && start[1] == destination[1] #up
+      up.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    elsif start[1] > destination[1] && start[0] == destination[0] #left
+      left.each do |move|
+        position = [(start[0] + move[0]), (start[1] + move[1])]
+        break if position == destination
+        path.push(position)
+      end
+    end
+    path.unshift(start)
+    path.push(destination)
+  end
 end
 
 # king = King.new([0, 4], 'b')
-# p king.possible_moves([0, 4])
+# p king.path([4, 4], [6,5])
